dfe339c0b53144fbe51a76b2f6e5d92f0501cbde
THangg06 <hanggnguyenn470@gmail.com>
2024-04-21 00:31:02 +0700

    Thêm dữ liệu từ thư mục C:/ProjectWF/PBL3

---
+++ Demo/ModelViews/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.ModelViews
+{
+    public class LoginViewModel
+    {
+        [MaxLength(150)]
+        [DataType(DataType.EmailAddress)]
+        [Remote(action: "ValidateEmail", controller: "Accounts ")]
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        public string Email { get; set; }
+
+        [MinLength(5, ErrorMessage ="Bạn cần nhập mật khẩu tối thiểu 5 kí tự")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [Display(Name = "Mật Khẩu")]
+        public string Password { get; set; }
+
+    }
+}
