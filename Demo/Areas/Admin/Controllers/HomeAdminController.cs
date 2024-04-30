29c647844e2524ce39ca5310b244404e454fb7d8
THangg06 <hanggnguyenn470@gmail.com>
2024-04-30 20:41:08 +0700

    admin

---
+++ Demo/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("Admin/HomeAdmin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -48,8 +48,17 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+ app.UseEndpoints(endpoints =>
+{
+    
+    endpoints.MapControllerRoute(
+        name: "admin",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+ 
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
