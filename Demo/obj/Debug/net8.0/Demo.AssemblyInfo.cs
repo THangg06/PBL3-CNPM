291840b58579107d140f31c0ac64b99b94e78d59
THangg06 <hanggnguyenn470@gmail.com>
2024-05-04 08:18:52 +0700

    chuyen

--- Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2da16b20ad8f4ecd342c6fee697ab7b76c5c331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a73bd1ce68292d7430cc9c679144816c8cb1413")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
