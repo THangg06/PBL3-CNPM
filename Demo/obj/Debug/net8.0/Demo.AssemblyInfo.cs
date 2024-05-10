f211444fb707021a39a346e1c5788729f405dfd6
MyDung04 <huynhmydung.17082004@gmail.com>
2024-05-11 02:59:13 +0700

    moi

--- Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7525a633bce6c0c42c72172f1864d7e038b442d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25fc6247fcd717e62fda4582d53da9813b4bd046")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
