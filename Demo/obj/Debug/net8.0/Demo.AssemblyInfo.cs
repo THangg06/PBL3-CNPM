6ec039529a2d229a55c8c94753f9d8d3b09e41c6
THangg06 <hanggnguyenn470@gmail.com>
2024-05-30 18:46:31 +0700

    cập nhật menu

--- Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4a8f5a0ff6f71873da2c57aeedecaa00d544b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d95bb6df8e0a15bf2a71da10ec1ae98d183b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
